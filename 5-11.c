b50a6b04ff0476ef6a1027238648d7ea2b6d6909
2wysl <162616040+2wysl@users.noreply.github.com>
2024-04-11 09:50:07 +0900

    240411

---
+++ 5-11.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+	int a, b;
+ 	char x;
+
+	printf("수식을 한줄로 띄어쓰기로 입력하세요 : ");
+	scanf("%d %c %d", &a, &x, &b);
+
+	switch(x)
+	{
+		case '+' : printf("%d + %d = %d 입니다.\n", a, b, a+b); break;
+		case '-' : printf("%d - %d = %d 입니다.\n", a, b, a-b); break;
+		case '*' : printf("%d * %d = %d 입니다.\n", a, b, a*b); break;
+		case '/' : printf("%d / %d = %d 입니다.\n", a, b, a/b); break;
+		case '%' : printf("%d %% %d = %d 입니다.\n", a, b, a%b); break;
+		default : printf("연산자를 잘못 입력했습니다.\n"); break;
+	}
+	
+}
+
