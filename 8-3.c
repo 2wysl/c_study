024f5031b961ebf71275ee7d571c09f01faf1867
2wysl <162616040+2wysl@users.noreply.github.com>
2024-05-03 20:48:54 +0900

    240503

---
+++ 8-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+	int aa[4];
+	int hap=0;
+	int i;
+
+	for(i=0; i<=3; i++)
+	{
+		printf("%d번째 숫자를 입력하세요 : ", i+1);
+		scanf("%d", &aa[i]);
+	}
+	
+	for(i=0; i<=3; i++)
+	{
+		hap=hap+aa[i];
+	}
+
+	printf("합계 ==> %d \n", hap);
+}
+
